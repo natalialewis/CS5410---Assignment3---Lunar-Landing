f17d07060db781591c1ceb3d82f76b7f1ac8dcef
Natalia Lewis <a02296603@usu.edu>
2025-03-02 00:21:11 -0700

    Added rendering background to game model

--- src/core/GameModel.java
+++ src/core/GameModel.java
@@ -1,4 +1,20 @@
 package core;
 
+import edu.usu.graphics.Graphics2D;
+import edu.usu.graphics.Texture;
+import ecs.systems.*;
+
+
 public class GameModel {
+    private ecs.systems.BackgroundRenderer backgroundRenderer;
+
+    public void initialize(Graphics2D graphics) {
+        var texBackground = new Texture("resources/images/background.jpg");
+
+        backgroundRenderer = new BackgroundRenderer(graphics, texBackground);
+    }
+
+    public void update(double elapsedTime) {
+        backgroundRenderer.update(elapsedTime);
+    }
 }
