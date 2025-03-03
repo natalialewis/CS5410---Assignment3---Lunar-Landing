e820bd8f1002b5c500b4f5a389e9aa4b6da1703d
Natalia Lewis <a02296603@usu.edu>
2025-03-02 18:48:46 -0700

    Created a terrain point component that stores the terrain points and the final terrain points as well as has getter and setter methods

--- src/ecs/components/TerrainPoints.java
+++ src/ecs/components/TerrainPoints.java
@@ -1,4 +1,40 @@
 package ecs.components;
 
-public class TerrainPoints {
+import java.util.ArrayList;
+import java.util.Random;
+
+public class TerrainPoints extends Component {
+    public ArrayList<ArrayList<Float>> terrainPoints = new ArrayList<>();
+    public ArrayList<ArrayList<Float>> terrainFinal = new ArrayList<>();
+
+    public float surfaceRoughness = 1.0f;  // This changes over time
+
+    public void add(ArrayList<Float> newPoint) {
+        terrainPoints.add(newPoint);
+    }
+
+    public ArrayList<Float> get(int index) {
+        return terrainPoints.get(index);
+    }
+
+    public ArrayList<ArrayList<Float>> getTerrainPoints() {
+        return terrainPoints;
+    }
+
+    public int size() {
+        return terrainPoints.size();
+
+    }
+
+    public void insert(int index, ArrayList<Float> point) {
+        terrainPoints.add(index, point);
+    }
+
+    public ArrayList<ArrayList<Float>> getTerrainFinal() {
+        return terrainFinal;
+    }
+
+    public void setTerrainFinal(ArrayList<ArrayList<Float>> terrainFinal) {
+        this.terrainFinal = terrainFinal;
+    }
 }
