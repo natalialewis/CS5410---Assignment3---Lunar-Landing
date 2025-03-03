18941f36f1f6945f1fb96f19550eada1f1d466bc
Natalia Lewis <a02296603@usu.edu>
2025-03-03 01:17:37 -0700

    Made image final

--- src/ecs/components/LanderAppearance.java
+++ src/ecs/components/LanderAppearance.java
@@ -3,7 +3,7 @@ package ecs.components;
 import edu.usu.graphics.Texture;
 
 public class LanderAppearance extends Component {
-    private Texture image;
+    private final Texture image;
 
     public LanderAppearance(Texture image) {
         this.image = image;
