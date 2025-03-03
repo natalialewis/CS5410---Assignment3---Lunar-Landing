83785439fea8719fa2afe9c17317210e3513b5e9
Natalia Lewis <a02296603@usu.edu>
2025-03-02 18:49:25 -0700

    Updated game model to include rendering the terrain

--- src/core/GameModel.java
+++ src/core/GameModel.java
@@ -1,20 +1,29 @@
 package core;
 
+import ecs.entities.Entity;
+import ecs.entities.Terrain;
 import edu.usu.graphics.Graphics2D;
 import edu.usu.graphics.Texture;
 import ecs.systems.*;
 
+import java.lang.System;
+
 
 public class GameModel {
     private ecs.systems.BackgroundRenderer backgroundRenderer;
+    private ecs.systems.TerrainRenderer terrainRenderer;
 
     public void initialize(Graphics2D graphics) {
         var texBackground = new Texture("resources/images/background.jpg");
 
         backgroundRenderer = new BackgroundRenderer(graphics, texBackground);
+        terrainRenderer = new TerrainRenderer(graphics);
+
+        terrainRenderer.add(Terrain.create());
     }
 
     public void update(double elapsedTime) {
         backgroundRenderer.update(elapsedTime);
+        terrainRenderer.update(elapsedTime);
     }
 }
