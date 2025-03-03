3c18619d310372a9b275de0d074ee9bca018f3b5
Natalia Lewis <a02296603@usu.edu>
2025-03-03 01:18:21 -0700

    Updated lunar lander entity with movement component

--- src/ecs/entities/Lander.java
+++ src/ecs/entities/Lander.java
@@ -1,4 +1,19 @@
 package ecs.entities;
 
+import edu.usu.graphics.Texture;
+import org.joml.Vector2f;
+
 public class Lander {
+
+    public static Entity create(Texture image, float x, float y, float angle, Vector2f center, float velocityX,
+                                float velocityY, float gravity) {
+        var lander = new Entity();
+
+        lander.add(new ecs.components.LanderAppearance(image));
+        lander.add(new ecs.components.LanderPosition(x, y, angle, center));
+        lander.add(new ecs.components.LanderMovement(velocityX, velocityY, gravity));
+//        lander.add(new ecs.components.LanderFuel(fuel));
+
+        return lander;
+    }
 }
