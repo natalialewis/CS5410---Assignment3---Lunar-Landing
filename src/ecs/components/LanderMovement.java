7919bc5989f0d0c8ad24358e0402ee07ffaa616f
Natalia Lewis <a02296603@usu.edu>
2025-03-03 01:09:52 -0700

    Created component for lander movement. Contains velocity in both directions and gravity

--- src/ecs/components/LanderMovement.java
+++ src/ecs/components/LanderMovement.java
@@ -1,4 +1,35 @@
 package ecs.components;
 
-public class LanderMovement {
+import edu.usu.graphics.Texture;
+
+public class LanderMovement extends Component{
+    private float velocityX;
+    private float velocityY;
+    private final float gravity;
+
+    public LanderMovement(float velocityX, float velocityY, float gravity) {
+        this.velocityX = velocityX;
+        this.velocityY = velocityY;
+        this.gravity = gravity;
+    }
+
+    public void setVelocityY(float velocityY) {
+        this.velocityY = velocityY;
+    }
+
+    public float getVelocityY() {
+        return this.velocityY;
+    }
+
+    public void setVelocityX(float velocityX) {
+        this.velocityX = velocityX;
+    }
+
+    public float getVelocityX() {
+        return this.velocityX;
+    }
+
+    public float getGravity() {
+        return this.gravity;
+    }
 }
