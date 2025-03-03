52670e02d4316eee19ec967efe0d613188905b13
Natalia Lewis <a02296603@usu.edu>
2025-03-03 01:16:16 -0700

    Created lander fuel component which just stores and updated fuel level

--- src/ecs/components/LanderFuel.java
+++ src/ecs/components/LanderFuel.java
@@ -1,4 +1,17 @@
 package ecs.components;
 
-public class LanderFuel {
+public class LanderFuel extends Component {
+    private float fuel;
+
+    public LanderFuel(float fuel) {
+        this.fuel = fuel;
+    }
+
+    public float getFuel() {
+        return this.fuel;
+    }
+
+    public void setFuel(float fuel) {
+        this.fuel = fuel;
+    }
 }
