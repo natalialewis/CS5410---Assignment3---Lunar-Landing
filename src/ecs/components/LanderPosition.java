c905f83547afc009767699f170a92f37350fb10b
Natalia Lewis <a02296603@usu.edu>
2025-03-03 01:12:57 -0700

    Created component for lander position. Includes x coordinate, y coordinate, angle, and center

--- src/ecs/components/LanderPosition.java
+++ src/ecs/components/LanderPosition.java
@@ -1,4 +1,38 @@
 package ecs.components;
 
-public class LanderPosition {
+import org.joml.Vector2f;
+
+public class LanderPosition extends Component{
+    public float x;
+    public float y;
+    public float angle;
+    public Vector2f center;
+
+    public LanderPosition(float x, float y, float angle, Vector2f center) {
+        this.x = x;
+        this.y = y;
+        this.angle = angle;
+        this.center = center;
+    }
+
+    public void setX(float x) {
+        this.x = x;
+    }
+
+    public float getX() {
+        System.out.println("X: " + x);
+        return x;
+    }
+
+    public void setY(float y) {
+        this.y = y;
+    }
+
+    public float getY() {
+        return y;
+    }
+
+    public float getAngle() {
+        return angle;
+    }
 }
