dcf4e477907f00c8d54143ce19b1d5624377c1ce
Natalia Lewis <a02296603@usu.edu>
2025-03-03 22:12:03 -0700

    Implemented speed in HUD

--- src/ecs/systems/LanderRenderer.java
+++ src/ecs/systems/LanderRenderer.java
@@ -1,11 +1,14 @@
 package ecs.systems;
 
 import ecs.components.LanderAppearance;
+import ecs.components.LanderFuel;
 import ecs.components.LanderMovement;
 import ecs.components.LanderPosition;
 import edu.usu.graphics.Color;
+import edu.usu.graphics.Font;
 import edu.usu.graphics.Graphics2D;
 import edu.usu.graphics.Rectangle;
+import views.MainMenuView;
 
 public class LanderRenderer extends System {
 
@@ -22,13 +25,35 @@ public class LanderRenderer extends System {
 
         for (var entity: entities.values()) {
             LanderPosition landerPosition = entity.get(ecs.components.LanderPosition.class);
+            LanderAppearance landerAppearance = entity.get(ecs.components.LanderAppearance.class);
             LanderMovement movement = entity.get(LanderMovement.class);
+            LanderFuel fuel = entity.get(LanderFuel.class);
 
             // Rocket
             Rectangle rocket = new Rectangle(landerPosition.getX(), landerPosition.getY(), 0.07f, 0.07f,
                     1.0f);
             graphics.draw(entity.get(LanderAppearance.class).getImage(), rocket, landerPosition.getAngle(),
                     landerPosition.getCenter(), Color.WHITE);
+
+            // Hud
+            final float fuelLeft = fuel.getFuel();
+            final float angleDegrees = Float.parseFloat(String.format("%.2f", (Math.toDegrees(landerPosition.getAngle()) + 360) % 360));
+            final float speed = Float.parseFloat(String.format("%.3f", movement.getVelocityY() * 50));
+
+            final float HUD_Height = 0.028f;
+            float left = 0.28f;
+            float top = -0.4f;
+
+            top = renderHUDItem(landerAppearance.getFont(), "fuel      :  " + fuelLeft + " s", left, top, HUD_Height, fuelLeft >= 0.00f ? Color.GREEN : Color.WHITE);
+            top = renderHUDItem(landerAppearance.getFont(), "speed  :  " + speed + " m/s", left, top, HUD_Height, speed <= 2.000f ? Color.GREEN : Color.WHITE);
+            renderHUDItem(landerAppearance.getFont(), "angle   :  " + angleDegrees + "°", left, top, HUD_Height, angleDegrees >= 355.00f || angleDegrees <= 5.00f ? Color.GREEN : Color.WHITE);
         }
+    }
+
+    private float renderHUDItem(Font font, String text, float left, float top, float height, Color color) {
+        float width = font.measureTextWidth(text, height);
+        graphics.drawTextByHeight(font, text, left, top, height, color);
+
+        return top + height;
     }
 }
