cb14d578f5100d402ff1d6671a0454ff8fddf766
Natalia Lewis <a02296603@usu.edu>
2025-03-03 22:04:07 -0700

    Added a font for lander renderer for the HUD

--- src/ecs/components/LanderAppearance.java
+++ src/ecs/components/LanderAppearance.java
@@ -1,15 +1,22 @@
 package ecs.components;
 
+import edu.usu.graphics.Font;
 import edu.usu.graphics.Texture;
 
 public class LanderAppearance extends Component {
     private final Texture image;
+    private final Font font;
 
-    public LanderAppearance(Texture image) {
+    public LanderAppearance(Texture image, Font font) {
         this.image = image;
+        this.font = font;
     }
 
     public Texture getImage() {
         return image;
+    }
+
+    public Font getFont() {
+        return font;
     }
 }
