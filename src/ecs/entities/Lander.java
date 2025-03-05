5f9e507927f03aa7d5a54906dee9bdb74f43b286
Natalia Lewis <a02296603@usu.edu>
2025-03-04 19:23:10 -0700

    Added width and height of lander to entity

--- src/ecs/entities/Lander.java
+++ src/ecs/entities/Lander.java
@@ -7,10 +7,12 @@ import org.joml.Vector2f;
 public class Lander {
 
     public static Entity create(Texture image, float x, float y, float angle, Vector2f center, float velocityX,
-                                float velocityY, float gravity, float thrust, Font font, float fuel) {
+                                float velocityY, float gravity, float thrust, Font font, float fuel, float width,
+                                float height) {
+
         var lander = new Entity();
 
-        lander.add(new ecs.components.LanderAppearance(image, font));
+        lander.add(new ecs.components.LanderAppearance(image, font, width, height));
         lander.add(new ecs.components.LanderPosition(x, y, angle, center));
         lander.add(new ecs.components.LanderMovement(velocityX, velocityY, gravity, thrust));
         lander.add(new ecs.components.LanderFuel(fuel));
