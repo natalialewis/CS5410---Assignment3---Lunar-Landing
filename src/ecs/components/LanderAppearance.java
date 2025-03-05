a1bb192f55fc67d30a3d119eac834a6ff735f340
Natalia Lewis <a02296603@usu.edu>
2025-03-04 19:25:51 -0700

    Added height and width property" "

--- src/ecs/components/LanderAppearance.java
+++ src/ecs/components/LanderAppearance.java
@@ -6,10 +6,14 @@ import edu.usu.graphics.Texture;
 public class LanderAppearance extends Component {
     private final Texture image;
     private final Font font;
+    private final float width;
+    private final float height;
 
-    public LanderAppearance(Texture image, Font font) {
+    public LanderAppearance(Texture image, Font font, float width, float height) {
         this.image = image;
         this.font = font;
+        this.width = width;
+        this.height = height;
     }
 
     public Texture getImage() {
@@ -18,5 +22,13 @@ public class LanderAppearance extends Component {
 
     public Font getFont() {
         return font;
+    }
+
+    public float getWidth() {
+        return width;
+    }
+
+    public float getHeight() {
+        return height;
     }
 }
