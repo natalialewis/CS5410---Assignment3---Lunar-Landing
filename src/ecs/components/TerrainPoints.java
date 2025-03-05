c36f86056e15649057ed3ee767661a7dac0a6859
Natalia Lewis <a02296603@usu.edu>
2025-03-04 18:49:04 -0700

    Fixed getters and setters and added a lines to store terrain lines for collisions

--- src/ecs/components/TerrainPoints.java
+++ src/ecs/components/TerrainPoints.java
@@ -1,14 +1,22 @@
 package ecs.components;
 
+import org.joml.Vector3f;
+
 import java.util.ArrayList;
-import java.util.Random;
 
 public class TerrainPoints extends Component {
-    public ArrayList<ArrayList<Float>> terrainPoints = new ArrayList<>();
-    public ArrayList<ArrayList<Float>> terrainFinal = new ArrayList<>();
-    public boolean level1 = true;
+    private ArrayList<ArrayList<Float>> terrainPoints;
+    private ArrayList<ArrayList<Float>> terrainFinal;
+    private ArrayList<ArrayList<Vector3f>> lines;
+    private float surfaceRoughness = 1.0f;
+    private boolean level1;
 
-    public float surfaceRoughness = 1.0f;  // This changes over time
+    public TerrainPoints() {
+        this.terrainPoints = new ArrayList<>();
+        this.terrainFinal = new ArrayList<>();
+        this.lines = new ArrayList<>();
+        this.level1 = true;
+    }
 
     public void add(ArrayList<Float> newPoint) {
         terrainPoints.add(newPoint);
@@ -39,7 +47,27 @@ public class TerrainPoints extends Component {
         this.terrainFinal = terrainFinal;
     }
 
+    public float getSurfaceRoughness() {
+        return surfaceRoughness;
+    }
+
+    public void setSurfaceRoughness(float surfaceRoughness) {
+        this.surfaceRoughness = surfaceRoughness;
+    }
+
     public void levelUp() {
         level1 = false;
+    }
+
+    public boolean isLevel1() {
+        return level1;
+    }
+
+    public void addLine(ArrayList<Vector3f> line) {
+        lines.add(line);
+    }
+
+    public ArrayList<ArrayList<Vector3f>> getLines() {
+        return lines;
     }
 }
