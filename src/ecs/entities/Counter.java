97d83a7a3d48b65dfa8c6790f2b16973892f0f41
Natalia Lewis <a02296603@usu.edu>
2025-03-06 00:00:22 -0700

    Implemented a counter entity for between levels

--- src/ecs/entities/Counter.java
+++ src/ecs/entities/Counter.java
@@ -1,4 +1,14 @@
 package ecs.entities;
 
+import edu.usu.graphics.Font;
+
 public class Counter {
+
+    public static Entity create(boolean state, Font font) {
+        var counter = new Entity();
+
+        counter.add(new ecs.components.Count(state, font));
+
+        return counter;
+    }
 }
