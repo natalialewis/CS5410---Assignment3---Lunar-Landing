e677d1ae61db7ff9a022ca9ba677776af437f324
Natalia Lewis <a02296603@usu.edu>
2025-03-06 00:00:04 -0700

    Created a count component to store data related to the counter between levels

--- src/ecs/components/Count.java
+++ src/ecs/components/Count.java
@@ -1,4 +1,48 @@
 package ecs.components;
 
-public class Count {
+import edu.usu.graphics.Font;
+
+public class Count extends Component {
+
+    private boolean countDown;
+    private float time;
+    private int count = 1;
+    private final Font font;
+
+    public Count(boolean count, Font font) {
+        this.countDown = count;
+        this.font = font;
+    }
+
+    public boolean getCountDown() {
+        return this.countDown;
+    }
+
+    public void setCountDown(boolean state) {
+        this.countDown = state;
+    }
+
+    public float getTime() {
+        return this.time;
+    }
+
+    public void setTime(float time) {
+        this.time = time;
+    }
+
+    public int getCount() {
+        return this.count;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public Font getFont() {
+        return this.font;
+    }
+
+    public void print(String text) {
+        System.out.println(text);
+    }
 }
