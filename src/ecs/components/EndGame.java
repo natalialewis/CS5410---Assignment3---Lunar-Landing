4c72e9f15e5a9015e389201f2aea5df8ee5942f1
Natalia Lewis <a02296603@usu.edu>
2025-03-06 14:29:44 -0700

    Made an endgame components to store if the game is over and to render score

--- src/ecs/components/EndGame.java
+++ src/ecs/components/EndGame.java
@@ -1,4 +1,33 @@
 package ecs.components;
 
-public class EndGame {
+public class EndGame extends Component{
+    private boolean endGame = false;
+    private boolean safeLanding;
+    private float score = 0;
+
+    public EndGame() {}
+
+    public boolean isEndGame() {
+        return this.endGame;
+    }
+
+    public void setEndGame(boolean endGame) {
+        this.endGame = endGame;
+    }
+
+    public float getScore() {
+        return this.score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    public boolean isSafeLanding() {
+        return this.safeLanding;
+    }
+
+    public void setSafeLanding(boolean safeLanding) {
+        this.safeLanding = safeLanding;
+    }
 }
