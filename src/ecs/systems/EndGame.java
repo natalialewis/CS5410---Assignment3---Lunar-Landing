ce4552af3d3a649974a163e0ce406e2fac508507
Natalia Lewis <a02296603@usu.edu>
2025-03-06 14:30:48 -0700

    Created blank system to eventually handle calculatind and rendering the end of game score

--- src/ecs/systems/EndGame.java
+++ src/ecs/systems/EndGame.java
@@ -1,4 +1,20 @@
 package ecs.systems;
 
-public class EndGame {
+import edu.usu.graphics.Graphics2D;
+
+public class EndGame extends System {
+
+    private final Graphics2D graphics;
+
+    public EndGame(Graphics2D graphics) {
+        super(ecs.components.EndGame.class);
+
+        this.graphics = graphics;
+    }
+
+    @Override
+    public void update(double elapsedTime) {
+
+    }
+
 }
