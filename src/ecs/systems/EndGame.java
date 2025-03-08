75635a3577df2f1c06372785dca6fa67871717b2
Natalia Lewis <a02296603@usu.edu>
2025-03-08 11:41:57 -0700

    Implemented high score saving

--- src/ecs/systems/EndGame.java
+++ src/ecs/systems/EndGame.java
@@ -1,23 +1,70 @@
 package ecs.systems;
 
+import com.google.gson.reflect.TypeToken;
 import edu.usu.graphics.Color;
 import edu.usu.graphics.Graphics2D;
 
-public class EndGame extends System {
+import com.google.gson.Gson;
+import java.io.FileWriter;
+import java.io.FileReader;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
+public class EndGame extends System implements Runnable {
+
+    private enum Activity {
+        Nothing,
+        Load,
+        Save
+    }
+
+    private boolean savedScore = false;
+    private boolean done = false;
+    private final Lock lockSignal = new ReentrantLock();
+    private final Condition doSomething = lockSignal.newCondition();
+    private Activity doThis = Activity.Nothing;
+    private final Thread tInternal;
     private final Graphics2D graphics;
+    ecs.components.EndGame endGame;
 
     public EndGame(Graphics2D graphics) {
         super(ecs.components.EndGame.class);
 
         this.graphics = graphics;
+        this.tInternal = new Thread(this);
+        tInternal.start();
+    }
+
+    @Override
+    public void run() {
+        try {
+            while (!done) {
+                // Wait for a signal to do something
+                lockSignal.lock();
+                doSomething.await();
+                lockSignal.unlock();
+
+                // Based on what was requested, do something
+                switch (doThis) {
+                    case Activity.Nothing -> {}
+                    case Activity.Save -> saveSomething();
+                    case Activity.Load -> loadHighScores();
+                }
+            }
+        } catch (Exception ex) {
+            java.lang.System.out.printf("Something bad happened: %s\n", ex.getMessage());
+        }
     }
 
     @Override
     public void update(double elapsedTime) {
 
         for (var entity: entities.values()) {
-            ecs.components.EndGame endGame = entity.get(ecs.components.EndGame.class);
+           endGame = entity.get(ecs.components.EndGame.class);
 
             if (endGame.isEndGame() && endGame.getScore() == 0.0f) {
                 float score = getScore(endGame.isSafeLanding(), endGame.getFuelLeft(), endGame.getLevel());
@@ -26,6 +73,14 @@ public class EndGame extends System {
 
             if (endGame.isEndGame()) {
                 renderScore(endGame);
+
+                if (!savedScore) {
+                    lockSignal.lock();
+                    doThis = Activity.Save;
+                    doSomething.signal();
+                    lockSignal.unlock();
+                    savedScore = true;
+                }
             }
         }
     }
@@ -44,4 +99,62 @@ public class EndGame extends System {
         graphics.drawTextByHeight(endGame.getFont(), String.format("Score: %.0f", endGame.getScore()), 0.29f, -0.46f, 0.04f, 1.0f, color);
     }
 
+    private synchronized void saveSomething() {
+
+        // Loads existing high scores
+        List<String> highScores = loadHighScores();
+
+        // Adds the new score
+        highScores.add(String.format("%.0f", endGame.getScore()));
+
+        // Sorts the high scores to see which one to get rid of
+        highScores.sort(Collections.reverseOrder());
+
+        if (highScores.size() > 5) {
+            highScores.remove(5);
+        }
+
+        // Saves highscores to the file
+        try (FileWriter writer = new FileWriter("highscores.json")) {
+            Gson gson = new Gson();
+            gson.toJson(highScores, writer);
+        } catch (Exception ex) {
+            java.lang.System.out.println("Error saving high scores: " + ex.getMessage());
+        }
+    }
+
+    private synchronized List<String> loadHighScores() {
+
+        // Existing high scores
+        List<String> highScores = new ArrayList<>();
+
+
+        // Loads the high scores from the file
+        try (FileReader reader = new FileReader("highscores.json")) {
+            Gson gson = new Gson();
+            highScores = gson.fromJson(reader,new TypeToken<List<String>>(){}.getType());
+
+            return highScores;
+        } catch (Exception ex) {
+            java.lang.System.out.println("Error loading: " + ex.getMessage());
+        }
+
+        return highScores;
+    }
+
+    public void shutdown() {
+        try {
+            lockSignal.lock();
+
+            doThis = Activity.Nothing;
+            done = true;
+            doSomething.signal();
+
+            lockSignal.unlock();
+
+            tInternal.join();
+        } catch (Exception ex) {
+            java.lang.System.out.printf("Failure to gracefully shutdown thread: %s\n", ex.getMessage());
+        }
+    }
 }
